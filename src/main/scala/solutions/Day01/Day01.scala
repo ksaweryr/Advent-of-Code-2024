efa1e93f1fab3af7309795ee0e416eec6ecbf93a
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-01 09:08:57 +0100

    Day 01

--- src/main/scala/solutions/Day01/Day01.scala
+++ src/main/scala/solutions/Day01/Day01.scala
@@ -1,4 +1,8 @@
 package solutions.Day01
 
 def solve(input: String): Unit =
-    println("TBI!")
+    val (a, b) = input.strip.split("\n").map(_.split("\\s+") match { case Array(x, y, _*) => (x.toInt, y.toInt)}).toList.unzip
+    val part1 = a.sorted.zip(b.sorted).map((x, y) => (x - y).abs).sum
+    println(part1)
+    val part2 = a.map(x => x * b.count(_ == x)).sum
+    println(part2)
