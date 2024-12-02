e0e6dc558a2303e907db5dffac8dedc7e4295544
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-02 08:56:28 +0100

    Day 02 part 1

--- src/main/scala/solutions/Day02/Day02.scala
+++ src/main/scala/solutions/Day02/Day02.scala
@@ -1,4 +1,9 @@
 package solutions.Day02
 
 def solve(input: String): Unit =
-    println("TBI!")
+    val reports = input.strip.split("\n").map(row => row.split("\\s+").map(_.toInt))
+    println(reports.count(isSafe))
+
+def isSafe(report: Array[Int]): Boolean =
+    val differences = report.zip(report.tail).map((a, b) => b - a)
+    differences.groupBy(_.sign).size == 1 && differences.forall(d => d.abs >= 1 && d.abs <= 3)
