80cea73e3002f82be59a6a90eba46828a482d255
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-02 09:37:39 +0100

    Day 02 part 2

--- src/main/scala/solutions/Day02/Day02.scala
+++ src/main/scala/solutions/Day02/Day02.scala
@@ -1,9 +1,16 @@
 package solutions.Day02
 
+import scala.util.Try
+
 def solve(input: String): Unit =
     val reports = input.strip.split("\n").map(row => row.split("\\s+").map(_.toInt))
     println(reports.count(isSafe))
+    println(reports.count(isSafe2))
 
 def isSafe(report: Array[Int]): Boolean =
     val differences = report.zip(report.tail).map((a, b) => b - a)
     differences.groupBy(_.sign).size == 1 && differences.forall(d => d.abs >= 1 && d.abs <= 3)
+
+def isSafe2(report: Array[Int]): Boolean =
+    (for i <- 0 to report.size
+        yield report.zipWithIndex.filter(_._2 != i).map(_._1)).exists(isSafe)
--- src/test/scala/Day02.scala
+++ src/test/scala/Day02.scala
@@ -1,4 +1,10 @@
 import solutions.Day02
 
 class Day02 extends munit.FunSuite:
-    
+    test("Part 2 already safe") {
+        assert(Day02.isSafe2(Array(7, 6, 4, 2, 1)))
+    }
+
+    test("Part 2 increasing-decreasing") {
+        assert(Day02.isSafe2(Array(1, 3, 2, 4, 5)))
+    }
