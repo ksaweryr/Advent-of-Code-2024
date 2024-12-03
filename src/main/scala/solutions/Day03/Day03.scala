991ea641e1c3505c3108edb37c78c189f01bfbec
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-03 09:20:39 +0100

    Day 03 part 2

--- src/main/scala/solutions/Day03/Day03.scala
+++ src/main/scala/solutions/Day03/Day03.scala
@@ -1,7 +1,23 @@
 package solutions.Day03
 
 def solve(input: String): Unit =
+    println(part1(input))
+    println(part2(input))
+
+def part1(input: String): Int =
     val pattern = "mul\\((\\d{1,3}),(\\d{1,3})\\)".r
-    val part1 = (for m <- pattern.findAllMatchIn(input)
+    (for m <- pattern.findAllMatchIn(input)
         yield m.group(1).toInt * m.group(2).toInt).sum
-    println(part1)
+
+def part2(input: String): Int =
+    val pattern = "(?:(mul)\\((\\d{1,3}),(\\d{1,3})\\))|(?:(do)\\(\\))|(?:(don't)\\(\\))".r
+    pattern.findAllMatchIn(input).foldLeft((0, 1))((acc, m) =>
+        if m.group(1) != null then
+            (acc._1 + m.group(2).toInt * m.group(3).toInt * acc._2, acc._2)
+        else if m.group(4) != null then
+            (acc._1, 1)
+        else if m.group(5) != null then
+            (acc._1, 0)
+        else
+            throw new RuntimeException("unreachable")
+    )._1
--- src/test/scala/Day03.scala
+++ src/test/scala/Day03.scala
@@ -1,4 +1,6 @@
 import solutions.Day03
 
 class Day03 extends munit.FunSuite:
-    
+    test("Part 2 example") {
+        assertEquals(Day03.part2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"), 48)
+    }
