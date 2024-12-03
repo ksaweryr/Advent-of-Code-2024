d725ab876a412eddbd6104b3fb71ad48ab020361
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-03 09:03:37 +0100

    Day 03 part 1

--- src/main/scala/solutions/Day03/Day03.scala
+++ src/main/scala/solutions/Day03/Day03.scala
@@ -1,4 +1,7 @@
 package solutions.Day03
 
 def solve(input: String): Unit =
-    println("TBI!")
+    val pattern = "mul\\((\\d{1,3}),(\\d{1,3})\\)".r
+    val part1 = (for m <- pattern.findAllMatchIn(input)
+        yield m.group(1).toInt * m.group(2).toInt).sum
+    println(part1)
