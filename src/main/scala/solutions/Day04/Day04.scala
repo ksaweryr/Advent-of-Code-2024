7b4552264abcfa75e2184482f616b3c302c288d5
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-04 09:20:55 +0100

    Day 04 part 1

--- src/main/scala/solutions/Day04/Day04.scala
+++ src/main/scala/solutions/Day04/Day04.scala
@@ -1,4 +1,35 @@
 package solutions.Day04
 
 def solve(input: String): Unit =
-    println("TBI!")
+    val grid = input.strip().split("\n").map(_.toArray)
+    println(part1(grid))
+
+def part1(grid: Array[Array[Char]]): Int =
+    (for { y <- grid.indices; x <- grid(0).indices } yield xmasCount(grid, (x,y))).sum
+
+def xmasCount(grid: Array[Array[Char]], position: (Int, Int)): Int =
+    val (x, y) = position
+    val canHaveHorizontal = x + 3 < grid(0).length
+    val canHaveVertical = y + 3 < grid.length
+
+    val horizontal = if canHaveHorizontal then
+        val word = grid(y).slice(x, x + 4).mkString
+        if word == "XMAS" || word == "SAMX" then 1 else 0
+    else 0
+
+    val vertical = if canHaveVertical then
+        val word = grid.slice(y, y + 4).map(_(x)).mkString
+        if word == "XMAS" || word == "SAMX" then 1 else 0
+    else 0
+
+    val diagonalUp = if canHaveHorizontal && y - 3 >= 0 then
+        val word = grid.slice(y - 3, y + 1).reverse.zipWithIndex.map((elem, idx) => elem(x + idx)).mkString
+        if word == "XMAS" || word == "SAMX" then 1 else 0
+    else 0
+
+    val diagonalDown = if canHaveHorizontal && canHaveVertical then
+        val word = grid.slice(y, y + 4).zipWithIndex.map((elem, idx) => elem(x + idx)).mkString
+        if word == "XMAS" || word == "SAMX" then 1 else 0
+    else 0
+
+    horizontal + vertical + diagonalUp + diagonalDown
--- src/test/scala/Day04.scala
+++ src/test/scala/Day04.scala
@@ -1,4 +1,8 @@
 import solutions.Day04
 
 class Day04 extends munit.FunSuite:
-    
+    test("Part 1 example") {
+        val input = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX\n"
+        val grid = input.strip().split("\n").map(_.toArray)
+        assertEquals(Day04.part1(grid), 18)
+    }
