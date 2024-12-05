112bed97bf90c273af071cbe185777e3d41f5d0a
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-05 10:13:12 +0100

    Improve topological sorting

--- src/main/scala/solutions/Day05/Day05.scala
+++ src/main/scala/solutions/Day05/Day05.scala
@@ -37,18 +37,17 @@ def topoSort(update: Array[Int], rules: Array[(Int, Int)]): Array[Int] =
     val filteredRules = rules.filter((a, b) => update.contains(a) && update.contains(b))
     val successors = filteredRules.groupBy(_._1).map((k, v) => (k, v.map(_._2)))
     var predecessorCounts = filteredRules.groupBy(_._2).map((k, v) => (k, v.size)).to(collection.mutable.Map)
-    for page <- update do
-        if !predecessorCounts.contains(page) then
-            predecessorCounts(page) = 0
 
     var sorted = collection.mutable.ArrayBuffer.empty[Int]
+    var ready = collection.mutable.Queue.from(update.filter(predecessorCounts.get(_).getOrElse(0) == 0))
 
-    while predecessorCounts.size > 0 do
-        val page = predecessorCounts.find(_._2 == 0).get._1
+    while ready.size > 0 do
+        val page = ready.dequeue
         sorted += page
         for { suc <- successors.lift(page).getOrElse(Array[Int]()) } do
             if predecessorCounts.contains(suc) then
                 predecessorCounts(suc) = predecessorCounts(suc) - 1
-        predecessorCounts.remove(page)
+                if predecessorCounts(suc) == 0 then
+                    ready.enqueue(suc)
     
     sorted.toArray
