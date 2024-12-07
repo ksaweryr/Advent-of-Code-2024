38d9b9a45c187d9655be10f0d63a5f82c2ffa124
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-07 09:46:58 +0100

    Day 07 part 1

--- src/main/scala/solutions/Day07/Day07.scala
+++ src/main/scala/solutions/Day07/Day07.scala
@@ -1,4 +1,22 @@
 package solutions.Day07
 
 def solve(input: String): Unit =
-    println("TBI!")
+    val equations = input.split("\n").map(parseEquation)
+    println(part1(equations))
+
+def part1(equations: Array[Equation]): Long =
+    equations.filter(_.satisfiable()).map(_.result).sum
+
+class Equation(val result: Long, val operands: List[Long]):
+    def satisfiable(acc: Option[Long] = None): Boolean =
+        if acc.getOrElse(-1L) > result then
+            false
+        else
+            operands match {
+                case head :: next => Equation(result, next).satisfiable(Some(acc.getOrElse(0L) + head)) || Equation(result, next).satisfiable(Some(acc.getOrElse(1L) * head))
+                case Nil => result == acc.getOrElse(-1L)
+            }
+
+def parseEquation(line: String): Equation =
+    val Array(a, b) = line.split(": ")
+    Equation(a.toLong, b.split(" ").map(_.toLong).toList)
--- src/test/scala/Day07.scala
+++ src/test/scala/Day07.scala
@@ -1,4 +1,8 @@
 import solutions.Day07
 
 class Day07 extends munit.FunSuite:
-    
+    test("Part 1 example") {
+        val input = "190: 10 19\n3267: 81 40 27\n83: 17 5\n156: 15 6\n7290: 6 8 6 15\n161011: 16 10 13\n192: 17 8 14\n21037: 9 7 18 13\n292: 11 6 16 20"
+        val equations = input.split("\n").map(Day07.parseEquation)
+        assertEquals(Day07.part1(equations), 3749L)
+    }
