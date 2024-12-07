3dc29cc49c27434e2d1a6e00dc7d8b2865ccb298
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-07 09:50:53 +0100

    Day 07 part 2

--- src/main/scala/solutions/Day07/Day07.scala
+++ src/main/scala/solutions/Day07/Day07.scala
@@ -3,9 +3,13 @@ package solutions.Day07
 def solve(input: String): Unit =
     val equations = input.split("\n").map(parseEquation)
     println(part1(equations))
+    println(part2(equations))
 
 def part1(equations: Array[Equation]): Long =
     equations.filter(_.satisfiable()).map(_.result).sum
+
+def part2(equations: Array[Equation]): Long =
+    equations.filter(_.satisfiable2()).map(_.result).sum
 
 class Equation(val result: Long, val operands: List[Long]):
     def satisfiable(acc: Option[Long] = None): Boolean =
@@ -14,6 +18,15 @@ class Equation(val result: Long, val operands: List[Long]):
         else
             operands match {
                 case head :: next => Equation(result, next).satisfiable(Some(acc.getOrElse(0L) + head)) || Equation(result, next).satisfiable(Some(acc.getOrElse(1L) * head))
+                case Nil => result == acc.getOrElse(-1L)
+            }
+    
+    def satisfiable2(acc: Option[Long] = None): Boolean =
+        if acc.getOrElse(-1L) > result then
+            false
+        else
+            operands match {
+                case head :: next => Equation(result, next).satisfiable2(Some(acc.getOrElse(0L) + head)) || Equation(result, next).satisfiable2(Some(acc.getOrElse(1L) * head)) || Equation(result, next).satisfiable2(Some(acc.getOrElse(0L) * Math.pow(10, Math.log10(head).toInt + 1).toLong + head))
                 case Nil => result == acc.getOrElse(-1L)
             }
 
