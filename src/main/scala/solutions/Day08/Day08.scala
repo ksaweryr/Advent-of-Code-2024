d871eaaee033a67b20b222cbe9192a9ae65d1421
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-08 10:40:22 +0100

    Day 08 part 2

--- src/main/scala/solutions/Day08/Day08.scala
+++ src/main/scala/solutions/Day08/Day08.scala
@@ -5,12 +5,34 @@ def solve(input: String): Unit =
     val antennas = grid.zipWithIndex.flatMap((r, y) => r.zipWithIndex.filter(_._1 != '.').map((c, x) => Antenna(c, (x, y)))).groupBy(_.frequency)
     val part1 = antennas.values.toSet.flatMap(as => (for { x <- as; y <- as } yield if x == y then Set.empty[(Int, Int)] else antinodes(x, y)).flatten).filter(inBounds(grid, _))
     println(part1.size)
+    val part2 = antennas.values.toSet.flatMap(as => (for { x <- as; y <- as } yield if x == y then Set.empty[(Int, Int)] else antinodes2(grid, x, y)).flatten)
+    println(part2.size)
 
 def antinodes(a1: Antenna, a2: Antenna): Set[(Int, Int)] =
     val (x1, y1) = a1.position
     val (x2, y2) = a2.position
     val (dx, dy) = (x1 - x2, y1 - y2)
     Set((x1 + dx, y1 + dy), (x2 - dx, y2 - dy))
+
+def antinodes2(grid: Array[Array[Char]], a1: Antenna, a2: Antenna): Set[(Int, Int)] =
+    val (x1, y1) = a1.position
+    val (x2, y2) = a2.position
+    val (dx, dy) = (x1 - x2, y1 - y2)
+    val s1 = Seq.unfold((x1, y1))((sx, sy) => {
+        if inBounds(grid, (sx, sy)) then
+            val (nx, ny) = (sx + dx, sy + dy)
+            Some(((sx, sy), (nx, ny)))
+        else
+            None
+    }).toSet
+    val s2 = Seq.unfold((x1, y1))((sx, sy) => {
+        if inBounds(grid, (sx, sy)) then
+            val (nx, ny) = (sx - dx, sy - dy)
+            Some(((sx, sy), (nx, ny)))
+        else
+            None
+    }).toSet
+    s1 ++ s2
 
 class Antenna(val frequency: Char, val position: (Int, Int)):
     override def toString(): String =
