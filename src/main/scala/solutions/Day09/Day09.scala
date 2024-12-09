2ba6329fd7f0a4dbba05dad972e3d62cfc3817c0
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-09 10:49:16 +0100

    Day 09 part 2

--- src/main/scala/solutions/Day09/Day09.scala
+++ src/main/scala/solutions/Day09/Day09.scala
@@ -6,6 +6,11 @@ def solve(input: String): Unit =
     val disk = (if input.size % 2 != 0 then input.toList else input.toList.init).map(_.toLong - '0'.toLong)
     val ids = (0L to disk.size / 2).toList
     println(part1(disk, ids))
+    val elements = disk.scanLeft((0L, 0L))((state, elem) => (state._1 + state._2, elem)).tail
+    val (filesList, spacesList) = elements.zipWithIndex.partition(_._2 % 2 == 0)
+    val files = filesList.map(_._1).zipWithIndex.map((ps, id) => (id.toLong, File(ps._1, ps._2))).toMap
+    val spaces = spacesList.map(_._1)
+    println(part2(files, spaces, files.size - 1))
 
 @tailrec def part1(disk: List[Long], ids: List[Long], position: Long = 0L, isCurrentlySpace: Boolean = false, acc: Long = 0L): Long =
     disk match {
@@ -23,5 +28,29 @@ def solve(input: String): Unit =
         case Nil => acc
     }
 
+class File(val pos: Long, val size: Long)
+
+@tailrec def part2(files: Map[Long, File], spaces: List[(Long, Long)], currentId: Long): Long =
+    if currentId == 0 then
+        files.map((id, f) => id * intervalSum(f.pos, f.pos + f.size - 1)).sum
+    else
+        val file = files(currentId)
+        val spaceToUse = spaces.zipWithIndex.find((s, idx) => s._2 >= file.size && s._1 < file.pos)
+        spaceToUse match {
+            case None => part2(files, spaces, currentId - 1)
+            case Some((p, idx)) =>
+                val (spacePos, spaceSize) = p
+                if spaceSize == file.size then
+                    part2(files + ((currentId, File(spacePos, file.size))), deleteAtIndex(spaces, idx), currentId - 1)
+                else
+                    part2(files + ((currentId, File(spacePos, file.size))), replaceAtIndex(spaces, idx, ((spacePos + file.size, spaceSize - file.size))), currentId - 1)
+        }
+
 def intervalSum(from: Long, to: Long): Long =
     (from + to) * (to - from + 1) / 2L
+
+def deleteAtIndex[T](a: List[T], idx: Int): List[T] =
+    a.take(idx) ++ a.drop(idx + 1)
+
+def replaceAtIndex[T](a: List[T], idx: Int, v: T): List[T] =
+    a.take(idx) ++ (v :: a.drop(idx + 1))
--- src/test/scala/Day09.scala
+++ src/test/scala/Day09.scala
@@ -7,3 +7,13 @@ class Day09 extends munit.FunSuite:
         val ids = (0L to disk.size / 2).toList
         assertEquals(Day09.part1(disk, ids), 1928L)
     }
+
+    test("Part 2 example") {
+        val input = "2333133121414131402"
+        val disk = (if input.size % 2 != 0 then input.toList else input.toList.init).map(_.toLong - '0'.toLong)
+        val elements = disk.scanLeft((0L, 0L))((state, elem) => (state._1 + state._2, elem)).tail
+        val (filesList, spacesList) = elements.zipWithIndex.partition(_._2 % 2 == 0)
+        val files = filesList.map(_._1).zipWithIndex.map((ps, id) => (id.toLong, Day09.File(ps._1, ps._2))).toMap
+        val spaces = spacesList.map(_._1)
+        assertEquals(Day09.part2(files, spaces, files.size - 1), 2858L)
+    }
