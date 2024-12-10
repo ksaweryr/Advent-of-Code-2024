8937c6c132ae6aef0c6bc61806e2ab3f812847c1
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-10 09:11:57 +0100

    Day 10 part 2

--- src/main/scala/solutions/Day10/Day10.scala
+++ src/main/scala/solutions/Day10/Day10.scala
@@ -4,26 +4,32 @@ import scala.annotation.tailrec
 
 def solve(input: String): Unit =
     val grid = input.split("\n").map(_.toArray)
-    println(part1(grid))
+    val (part1, part2) = bothParts(grid)
+    println(part1)
+    println(part2)
 
-def part1(grid: Array[Array[Char]]): Int =
-    grid.map(_.zipWithIndex).zipWithIndex.flatMap((p, y) => p.filter(_._1 == '0').map((h, x) => (x, y))).map(trailheadScore(grid, _)).sum
+def bothParts(grid: Array[Array[Char]]): (Int, Int) =
+    val (part1, part2) = grid.map(_.zipWithIndex).zipWithIndex.flatMap((p, y) => p.filter(_._1 == '0').map((h, x) => (x, y))).map(s => (trailheadScore(grid, s), trailheadRating(grid, s))).unzip
+    (part1.sum, part2.sum)
 
 def trailheadScore(grid: Array[Array[Char]], start: (Int, Int)): Int =
-    dfs(grid, List(start))
+    dfs(grid, List(start), true)
 
-@tailrec def dfs(grid: Array[Array[Char]], toVisit: List[(Int, Int)], visited: Set[(Int, Int)] = Set.empty, ninesCount: Int = 0): Int =
+def trailheadRating(grid: Array[Array[Char]], start: (Int, Int)): Int =
+    dfs(grid, List(start), false)
+
+@tailrec def dfs(grid: Array[Array[Char]], toVisit: List[(Int, Int)], trackVisited: Boolean, visited: Set[(Int, Int)] = Set.empty, ninesCount: Int = 0): Int =
     toVisit match {
         case (x, y) :: next => 
             val currentChar = grid(y)(x)
             if currentChar == '9' then
-                dfs(grid, next, visited + ((x, y)), ninesCount + 1)
+                dfs(grid, next, trackVisited, visited + ((x, y)), ninesCount + 1)
             else
                 val newToVisit = Seq((-1, 0), (1, 0), (0, -1), (0, 1))
                     .map((dx, dy) => (x + dx, y + dy))
-                    .filter((nx, ny) => inBounds(grid, (nx, ny)) && grid(ny)(nx) == currentChar + 1 && !visited.contains((nx, ny)))
+                    .filter((nx, ny) => inBounds(grid, (nx, ny)) && grid(ny)(nx) == currentChar + 1 && (!trackVisited || !visited.contains((nx, ny))))
                     .toList
-                dfs(grid, newToVisit ++ next, visited + ((x, y)), ninesCount)
+                dfs(grid, newToVisit ++ next, trackVisited, visited + ((x, y)), ninesCount)
         case Nil => ninesCount
     }
 
--- src/test/scala/Day10.scala
+++ src/test/scala/Day10.scala
@@ -4,5 +4,5 @@ class Day10 extends munit.FunSuite:
     test("Part 1 small example") {
         val input = "0123\n1234\n8765\n9876"
         val grid = input.split("\n").map(_.toArray)
-        assertEquals(Day10.part1(grid), 1)
+        assertEquals(Day10.bothParts(grid)._1, 1)
     }
