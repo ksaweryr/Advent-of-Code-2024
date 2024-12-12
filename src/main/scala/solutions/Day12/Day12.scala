f1d7e86227a8f7ce8462f58f57435bac5df95379
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-12 07:31:01 +0100

    Day 12 part 1

--- src/main/scala/solutions/Day12/Day12.scala
+++ src/main/scala/solutions/Day12/Day12.scala
@@ -1,4 +1,34 @@
 package solutions.Day12
 
+import scala.annotation.tailrec
+
 def solve(input: String): Unit =
-    println("TBI!")
+    val grid = input.split("\n").map(_.toArray)
+    println(part1(grid))
+
+def part1(grid: Array[Array[Char]]): Int =
+    grid.zipWithIndex.flatMap((row, y) => row.zipWithIndex.map((c, x) => (x, y))).foldLeft((0, Set.empty[(Int, Int)]))((acc, pos) => {
+        val (totalCost, visited) = acc
+        if !visited.contains(pos) then
+            val (price, newVisited) = findPrice(grid, List(pos), visited + pos)
+            (totalCost + price, newVisited)
+        else
+            acc
+    })._1
+
+@tailrec def findPrice(grid: Array[Array[Char]], toVisit: List[(Int, Int)], visited: Set[(Int, Int)], acc: (Int, Int) = (0, 0)): (Int, Set[(Int, Int)]) =
+    toVisit match
+        case head :: next => {
+            val (x, y) = head
+            val c = grid(y)(x)
+            val newToVisit = List((1, 0), (-1, 0), (0, 1), (0, -1)).map((dx, dy) => (x + dx, y + dy)).filter(p => inBounds(grid, p) && grid(p._2)(p._1) == c)
+            val newPerimeter = 4 - newToVisit.size
+            val actualNewToVisit = newToVisit.filterNot(visited.contains(_))
+            findPrice(grid, actualNewToVisit ++ next, visited ++ actualNewToVisit.toSet, (acc._1 + 1, acc._2 + newPerimeter))
+        }
+        case Nil => (acc._1 * acc._2, visited)
+    
+
+def inBounds(grid: Array[Array[Char]], pos: (Int, Int)): Boolean =
+    val (x, y) = pos
+    y >= 0 && y < grid.size && x >= 0 && x < grid(0).size
--- src/test/scala/Day12.scala
+++ src/test/scala/Day12.scala
@@ -1,4 +1,7 @@
 import solutions.Day12
 
 class Day12 extends munit.FunSuite:
-    
+    test("Part 1 small example") {
+        val grid = "AAAA\nBBCD\nBBCC\nEEEC".split("\n").map(_.toArray)
+        assertEquals(Day12.part1(grid), 140)
+    }
