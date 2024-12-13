646664c91f1e77a60f398bf1939784facb61361b
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-13 08:14:39 +0100

    Day 13 part 1

--- src/main/scala/solutions/Day13/Day13.scala
+++ src/main/scala/solutions/Day13/Day13.scala
@@ -1,4 +1,54 @@
 package solutions.Day13
 
+import scala.annotation.tailrec
+
 def solve(input: String): Unit =
-    println("TBI!")
+    val machines = input.split("\n\n").map(parseMachine(_))
+    println(part1(machines))
+
+def part1(machines: Array[Machine]): Int =
+    machines.map(_.requiredTokens()).sum
+
+class Machine(val a: (Int, Int), val b: (Int, Int), val prize: (Int, Int)):
+    override def toString(): String =
+        s"Machine(Button A: ${a}, Button B: ${b}, Prize: ${prize})"
+    
+    def requiredTokens(): Int =
+        solveSystem((a._1, b._1, prize._1), (a._2, b._2, prize._2)) match {
+            case None => 0
+            case Some((x, y)) => if x <= 100 && y <= 100 then 3 * x + y else 0
+            case Some((a, b, c)) => throw RuntimeException("A system with infinite solutions doesn't appear in the input")
+        }
+
+def parseMachine(s: String): Machine =
+    val lines = s.split("\n")
+    Machine(parseButton(lines(0)), parseButton(lines(1)), parsePrize(lines(2)))
+
+def parseButton(s: String): (Int, Int) =
+    val parts = s.split(": ")(1).split(", ")
+    val x = parts(0).substring(1).toInt
+    val y = parts(1).substring(1).toInt
+    (x, y)
+
+def parsePrize(s: String): (Int, Int) =
+    val parts = s.split(": ")(1).split(", ")
+    val x = parts(0).substring(2).toInt
+    val y = parts(1).substring(2).toInt
+    (x, y)
+
+def solveSystem(e1: (Int, Int, Int), e2: (Int, Int, Int)): Option[(Int, Int) | (Int, Int, Int)] =
+    val (a1, b1, c1) = e1
+    val (a2, b2, c2) = e2
+    val d = a1 * b2 - a2 * b1
+    val dx = c1 * b2 - c2 * b1
+    val dy = a1 * c2 - a2 * c1
+
+    if d == 0 then
+        if dx == 0 && dy == 0 then
+            Some((a2 - a1, b2 - b1, c2 - c1))
+        else
+            None
+    else if dx % d != 0 || dy % d != 0 then
+        None
+    else
+        Some((dx / d, dy / d))
--- src/test/scala/Day13.scala
+++ src/test/scala/Day13.scala
@@ -1,4 +1,23 @@
 import solutions.Day13
 
 class Day13 extends munit.FunSuite:
-    
+    test("System of equations with one solution") {
+        val s = Day13.solveSystem((94, 22, 8400), (34, 67, 5400))
+        assertEquals(s, Some((80, 40)))
+    }
+
+    test("System of equations with no solutions") {
+        val s = Day13.solveSystem((26, 67, 12748), (66, 21, 12176))
+        assertEquals(s, None)
+    }
+
+    test("System of equations with infinite solutions") {
+        val s = Day13.solveSystem((1, 1, 1), (2, 2, 2))
+        assertEquals(s, Some((1, 1, 1)))
+    }
+
+    test("Part 1 example") {
+        val input = "Button A: X+94, Y+34\nButton B: X+22, Y+67\nPrize: X=8400, Y=5400\n\nButton A: X+26, Y+66\nButton B: X+67, Y+21\nPrize: X=12748, Y=12176\n\nButton A: X+17, Y+86\nButton B: X+84, Y+37\nPrize: X=7870, Y=6450\n\nButton A: X+69, Y+23\nButton B: X+27, Y+71\nPrize: X=18641, Y=10279"
+        val machines = input.split("\n\n").map(Day13.parseMachine(_))
+        assertEquals(Day13.part1(machines), 480)
+    }
