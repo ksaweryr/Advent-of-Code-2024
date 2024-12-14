5b4de4ba75c467100fdfe03aa7dc0fc4fb59a6b5
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-14 10:20:08 +0100

    Day 14 part 1

--- src/main/scala/solutions/Day14/Day14.scala
+++ src/main/scala/solutions/Day14/Day14.scala
@@ -1,4 +1,54 @@
 package solutions.Day14
 
 def solve(input: String): Unit =
-    println("TBI!")
+    val robots = input.split("\n").map(parseRobot(_, (101, 103)))
+    println(part1(robots))
+
+def part1(robots: Array[Robot]): Int =
+    robots
+        .map(_.move(100))
+        .groupBy(_.quadrant())
+        .filterKeys(_.isDefined)
+        .values
+        .map(_.length)
+        .fold(1)(_ * _)
+
+def parseRobot(s: String, bounds: (Int, Int)): Robot =
+    val parts = s.split(" ")
+    Robot(parseVector(parts(0)), parseVector(parts(1)), bounds)
+
+def parseVector(s: String): (Int, Int) =
+    val ns = s.split("=")(1).split(",")
+    (ns(0).toInt, ns(1).toInt)
+
+enum Quadrant:
+    case TL, TR, BL, BR
+
+class Robot(val p: (Int, Int), val v: (Int, Int), val bounds: (Int, Int)):
+    def move(steps: Int): Robot =
+        val (maxX, maxY) = bounds
+        val (x0, y0) = p
+        val (vx, vy) = v
+        Robot((mod(x0 + steps * vx, maxX), mod(y0 + steps * vy, maxY)), v, bounds)
+    def quadrant(): Option[Quadrant] =
+        val (x, y) = p
+        val (mx, my) = bounds
+        val left = x < mx / 2
+        val right = x > mx / 2
+        val top = y < my / 2
+        val bottom = y > my / 2
+        if left && top then
+            Some(Quadrant.TL)
+        else if left && bottom then
+            Some(Quadrant.BL)
+        else if right && top then
+            Some(Quadrant.TR)
+        else if right && bottom then
+            Some(Quadrant.BR)
+        else
+            None
+    override def toString(): String =
+        s"Robot(p=${p}, v=${v}, bounds=${bounds})"
+
+def mod(a: Int, m: Int): Int =
+    ((a % m) + m) % m
--- src/test/scala/Day14.scala
+++ src/test/scala/Day14.scala
@@ -1,4 +1,8 @@
 import solutions.Day14
 
 class Day14 extends munit.FunSuite:
-    
+    test("Part 1 example") {
+        val input = "p=0,4 v=3,-3\np=6,3 v=-1,-3\np=10,3 v=-1,2\np=2,0 v=2,-1\np=0,0 v=1,3\np=3,0 v=-2,-2\np=7,6 v=-1,-3\np=3,0 v=-1,-2\np=9,3 v=2,3\np=7,3 v=-1,2\np=2,4 v=2,-3\np=9,5 v=-3,-3"
+        val robots = input.split("\n").map(Day14.parseRobot(_, (11, 7)))
+        assertEquals(Day14.part1(robots), 12)
+    }
