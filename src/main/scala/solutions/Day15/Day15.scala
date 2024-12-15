3a4cf022bf0e16d654e874e17ae981d391fe6e30
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-15 11:01:33 +0100

    Day 15 part 1

--- src/main/scala/solutions/Day15/Day15.scala
+++ src/main/scala/solutions/Day15/Day15.scala
@@ -1,4 +1,69 @@
 package solutions.Day15
 
+import scala.annotation.tailrec
+
 def solve(input: String): Unit =
-    println("TBI!")
+    val parts = input.split("\n\n")
+    val (grid, robotPos) = parseGrid(parts(0))
+    val moves = parseMoves(parts(1))
+    println(part1(grid, robotPos, moves))
+
+@tailrec def part1(grid: Array[Array[Option[WarehouseObject]]], robotPos: (Int, Int), moves: List[Direction]): Int =
+    moves match
+        case head :: next => attemptMove(grid, robotPos, head) match
+            case None => part1(grid, robotPos, next)
+            case Some(newGrid) => part1(newGrid, newPosition(robotPos, head), next)
+        case Nil => grid
+            .zipWithIndex
+            .flatMap((row, y) => row
+                .zipWithIndex
+                .filter(_._1 == Some(WarehouseObject.Box))
+                .map((b, x) => x + y * 100))
+            .sum
+
+def attemptMove(grid: Array[Array[Option[WarehouseObject]]], pos: (Int, Int), direction: Direction): Option[Array[Array[Option[WarehouseObject]]]] =
+    val newpos = newPosition(pos, direction)
+    grid(newpos._2)(newpos._1) match
+        case None => 
+            var newGrid = grid
+            newGrid(newpos._2)(newpos._1) = grid(pos._2)(pos._1)
+            Some(newGrid)
+        case Some(WarehouseObject.Box) =>
+            attemptMove(grid, newpos, direction) match
+                case None => None
+                case Some(_newGrid) =>
+                    var newGrid = _newGrid
+                    newGrid(newpos._2)(newpos._1) = grid(pos._2)(pos._1)
+                    Some(newGrid) 
+        case Some(WarehouseObject.Wall) => None
+
+def newPosition(pos: (Int, Int), direction: Direction): (Int, Int) =
+    direction match
+        case Direction.Up => (pos._1, pos._2 - 1)
+        case Direction.Down => (pos._1, pos._2 + 1)
+        case Direction.Left => (pos._1 - 1, pos._2)
+        case Direction.Right => (pos._1 + 1, pos._2)
+
+enum WarehouseObject:
+    case Box, Wall
+
+enum Direction:
+    case Up, Down, Left, Right
+
+def parseGrid(s: String): (Array[Array[Option[WarehouseObject]]], (Int, Int)) =
+    val lines = s.split("\n")
+    val robotPos = lines.zipWithIndex.flatMap((l, y) => l.zipWithIndex.map((c, x) => (c, (x, y)))).find(_._1 == '@').get._2
+    val grid = lines.map(_.map(_ match {
+        case '#' => Some(WarehouseObject.Wall)
+        case 'O' => Some(WarehouseObject.Box)
+        case _ => None
+    }).toArray)
+    (grid, robotPos)
+
+def parseMoves(s: String): List[Direction] =
+    s.split("\n").mkString.map(_ match
+        case '^' => Direction.Up
+        case 'v' => Direction.Down
+        case '<' => Direction.Left
+        case '>' => Direction.Right
+    ).toList
--- src/test/scala/Day15.scala
+++ src/test/scala/Day15.scala
@@ -1,4 +1,10 @@
 import solutions.Day15
 
 class Day15 extends munit.FunSuite:
-    
+    test("Part 1 small example") {
+        val input = "########\n#..O.O.#\n##@.O..#\n#...O..#\n#.#.O..#\n#...O..#\n#......#\n########\n\n<^^>>>vv<v>>v<<"
+        val parts = input.split("\n\n")
+        val (grid, robotPos) = Day15.parseGrid(parts(0))
+        val moves = Day15.parseMoves(parts(1))
+        assertEquals(Day15.part1(grid, robotPos, moves), 2028)
+    }
