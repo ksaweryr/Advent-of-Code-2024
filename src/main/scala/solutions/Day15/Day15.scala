8f366dbd5d0d195a67d0bbf206b33e3be878b1ba
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-16 23:41:11 +0100

    Day 15 part 2

--- src/main/scala/solutions/Day15/Day15.scala
+++ src/main/scala/solutions/Day15/Day15.scala
@@ -5,10 +5,14 @@ import scala.annotation.tailrec
 def solve(input: String): Unit =
     val parts = input.split("\n\n")
     val (grid, robotPos) = parseGrid(parts(0))
+    val (grid2, robotPos2) = widenGrid(grid, robotPos)
     val moves = parseMoves(parts(1))
     println(part1(grid, robotPos, moves))
+    println(part2(grid2, robotPos2, moves))
 
-@tailrec def part1(grid: Array[Array[Option[WarehouseObject]]], robotPos: (Int, Int), moves: List[Direction]): Int =
+type Grid = Array[Array[Option[WarehouseObject]]]
+
+@tailrec def part1(grid: Grid, robotPos: (Int, Int), moves: List[Direction]): Int =
     moves match
         case head :: next => attemptMove(grid, robotPos, head) match
             case None => part1(grid, robotPos, next)
@@ -21,7 +25,21 @@ def solve(input: String): Unit =
                 .map((b, x) => x + y * 100))
             .sum
 
-def attemptMove(grid: Array[Array[Option[WarehouseObject]]], pos: (Int, Int), direction: Direction): Option[Array[Array[Option[WarehouseObject]]]] =
+@tailrec def part2(grid: Grid, robotPos: (Int, Int), moves: List[Direction]): Int =
+    val lastGrid = grid.map(row => row.clone)
+    moves match
+        case head :: next => attemptMove2(grid, robotPos, head) match
+            case None => part2(lastGrid, robotPos, next)
+            case Some((newGrid, _)) => part2(newGrid, newPosition(robotPos, head), next)
+        case Nil => grid
+            .zipWithIndex
+            .flatMap((row, y) => row
+                .zipWithIndex
+                .filter(_._1 == Some(WarehouseObject.LeftBox))
+                .map((b, x) => x + y * 100))
+            .sum
+
+def attemptMove(grid: Grid, pos: (Int, Int), direction: Direction): Option[Grid] =
     val newpos = newPosition(pos, direction)
     grid(newpos._2)(newpos._1) match
         case None => 
@@ -36,6 +54,40 @@ def attemptMove(grid: Array[Array[Option[WarehouseObject]]], pos: (Int, Int), di
                     newGrid(newpos._2)(newpos._1) = grid(pos._2)(pos._1)
                     Some(newGrid) 
         case Some(WarehouseObject.Wall) => None
+        case _ => throw RuntimeException("Should never happen")
+
+def attemptMove2(grid: Grid, pos: (Int, Int), direction: Direction, updated: Set[(Int, Int)] = Set.empty): Option[(Grid, Set[(Int, Int)])] =
+    val newpos = newPosition(pos, direction)
+    grid(newpos._2)(newpos._1) match
+        case None =>
+            var newGrid = grid
+            newGrid(newpos._2)(newpos._1) = grid(pos._2)(pos._1)
+            newGrid(pos._2)(pos._1) = None
+            Some((newGrid, updated + pos + newpos))
+        case Some(WarehouseObject.LeftBox) =>
+            attemptMove2(grid, newpos, direction, updated) match
+                case None => None
+                case Some((_newGrid, newUpdated)) =>
+                    var newGrid = _newGrid
+                    newGrid(newpos._2)(newpos._1) = grid(pos._2)(pos._1)
+                    newGrid(pos._2)(pos._1) = None
+                    if (direction == Direction.Up || direction == Direction.Down) && !newUpdated.contains((newpos._1 + 1, newpos._2)) then
+                        attemptMove2(newGrid, (newpos._1 + 1, newpos._2), direction, newUpdated + pos + newpos)
+                    else
+                        Some((newGrid, newUpdated + pos + newpos))
+        case Some(WarehouseObject.RightBox) =>
+            attemptMove2(grid, newpos, direction, updated) match
+                case None => None
+                case Some((_newGrid, newUpdated)) =>
+                    var newGrid = _newGrid
+                    newGrid(newpos._2)(newpos._1) = grid(pos._2)(pos._1)
+                    newGrid(pos._2)(pos._1) = None
+                    if (direction == Direction.Up || direction == Direction.Down) && !newUpdated.contains((newpos._1 - 1, newpos._2)) then
+                        attemptMove2(newGrid, (newpos._1 - 1, newpos._2), direction, newUpdated + pos + newpos)
+                    else
+                        Some((newGrid, newUpdated + pos + newpos))
+        case Some(WarehouseObject.Wall) => None
+        case _ => throw RuntimeException("Should never happen")
 
 def newPosition(pos: (Int, Int), direction: Direction): (Int, Int) =
     direction match
@@ -45,12 +97,12 @@ def newPosition(pos: (Int, Int), direction: Direction): (Int, Int) =
         case Direction.Right => (pos._1 + 1, pos._2)
 
 enum WarehouseObject:
-    case Box, Wall
+    case Box, Wall, LeftBox, RightBox
 
 enum Direction:
     case Up, Down, Left, Right
 
-def parseGrid(s: String): (Array[Array[Option[WarehouseObject]]], (Int, Int)) =
+def parseGrid(s: String): (Grid, (Int, Int)) =
     val lines = s.split("\n")
     val robotPos = lines.zipWithIndex.flatMap((l, y) => l.zipWithIndex.map((c, x) => (c, (x, y)))).find(_._1 == '@').get._2
     val grid = lines.map(_.map(_ match {
@@ -59,6 +111,23 @@ def parseGrid(s: String): (Array[Array[Option[WarehouseObject]]], (Int, Int)) =
         case _ => None
     }).toArray)
     (grid, robotPos)
+
+def widenGrid(grid: Grid, startPos: (Int, Int)): (Grid, (Int, Int)) =
+    (grid.map(row => row.flatMap(cell => cell match
+        case None => Seq(None, None)
+        case Some(WarehouseObject.Box) => Seq(Some(WarehouseObject.LeftBox), Some(WarehouseObject.RightBox))
+        case Some(WarehouseObject.Wall) => Seq(Some(WarehouseObject.Wall), Some(WarehouseObject.Wall))
+        case _ => throw RuntimeException("Should never happen")
+    )), (startPos._1 * 2, startPos._2))
+
+def dumpGrid(grid: Grid): String =
+    grid.map(row => row.map(_ match
+        case None => '.'
+        case Some(WarehouseObject.Box) => 'O'
+        case Some(WarehouseObject.LeftBox) => '['
+        case Some(WarehouseObject.RightBox) => ']'
+        case Some(WarehouseObject.Wall) => '#'
+    ).mkString).mkString("\n")
 
 def parseMoves(s: String): List[Direction] =
     s.split("\n").mkString.map(_ match
--- src/test/scala/Day15.scala
+++ src/test/scala/Day15.scala
@@ -8,3 +8,71 @@ class Day15 extends munit.FunSuite:
         val moves = Day15.parseMoves(parts(1))
         assertEquals(Day15.part1(grid, robotPos, moves), 2028)
     }
+
+    test("Part 2 small example") {
+        val input = "#######\n#...#.#\n#.....#\n#..OO@#\n#..O..#\n#.....#\n#######\n\n<vv<<^^<<^^"
+        val parts = input.split("\n\n")
+        val (_grid, _robotPos) = Day15.parseGrid(parts(0))
+        val (grid, robotPos) = Day15.widenGrid(_grid, _robotPos)
+        val moves = Day15.parseMoves(parts(1))
+        assertEquals(Day15.part2(grid, robotPos, moves), 618)
+    }
+
+    test("attemptMove2 push wide box") {
+        val input = "....\n....\n.O..\n.O..\n.@..\n....\n\n^"
+        val parts = input.split("\n\n")
+        val (_grid, _robotPos) = Day15.parseGrid(parts(0))
+        val (grid, robotPos) = Day15.widenGrid(_grid, _robotPos)
+
+        val nextGrid = Day15.attemptMove2(grid, robotPos, Day15.Direction.Up)
+
+        assert(nextGrid.isDefined)
+        assertEquals(Day15.dumpGrid(nextGrid.get._1), "........\n..[]....\n..[]....\n........\n........\n........")
+    }
+
+    test("attemptMove2 push wide pyramid") {
+        val input = "..#.\n.OO.\n..O@\n....\n\n<v<^"
+        val parts = input.split("\n\n")
+        val (_grid, _robotPos) = Day15.parseGrid(parts(0))
+        val (grid, robotPos) = Day15.widenGrid(_grid, _robotPos)
+
+        var nextGrid = Day15.attemptMove2(grid, robotPos, Day15.Direction.Left)
+        assert(nextGrid.isDefined)
+        nextGrid = Day15.attemptMove2(nextGrid.get._1, (robotPos._1 - 1, robotPos._2), Day15.Direction.Down)
+        assert(nextGrid.isDefined)
+        nextGrid = Day15.attemptMove2(nextGrid.get._1, (robotPos._1 - 1, robotPos._2 + 1), Day15.Direction.Left)
+        assert(nextGrid.isDefined)
+        assert(Day15.attemptMove2(nextGrid.get._1, (robotPos._1 - 2, robotPos._2 + 1), Day15.Direction.Up).isEmpty)
+
+        assertEquals(Day15.dumpGrid(nextGrid.get._1), "....##..\n..[][]..\n...[]...\n........")
+    }
+
+    test("attemptMove2 push one side into wall") {
+        val input = "..#.\n..O@\n....\n\n<v<<^"
+        val parts = input.split("\n\n")
+        val (_grid, _robotPos) = Day15.parseGrid(parts(0))
+        val (grid, robotPos) = Day15.widenGrid(_grid, _robotPos)
+
+        var nextGrid = Day15.attemptMove2(grid, robotPos, Day15.Direction.Left)
+        assert(nextGrid.isDefined)
+        nextGrid = Day15.attemptMove2(nextGrid.get._1, (robotPos._1 - 1, robotPos._2), Day15.Direction.Down)
+        assert(nextGrid.isDefined)
+        nextGrid = Day15.attemptMove2(nextGrid.get._1, (robotPos._1 - 1, robotPos._2 + 1), Day15.Direction.Left)
+        assert(nextGrid.isDefined)
+        nextGrid = Day15.attemptMove2(nextGrid.get._1, (robotPos._1 - 2, robotPos._2 + 1), Day15.Direction.Left)
+        assert(nextGrid.isDefined)
+        val finalGrid = nextGrid.get._1.map(row => row.clone)
+        assert(Day15.attemptMove2(nextGrid.get._1, (robotPos._1 - 3, robotPos._2 + 1), Day15.Direction.Up).isEmpty)
+
+        assertEquals(Day15.dumpGrid(finalGrid), "....##..\n...[]...\n........")
+    }
+
+    test("Part 2 large example") {
+        val input = "##########\n#..O..O.O#\n#......O.#\n#.OO..O.O#\n#..O@..O.#\n#O#..O...#\n#O..O..O.#\n#.OO.O.OO#\n#....O...#\n##########\n\n<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^\nvvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v\n><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<\n<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^\n^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><\n^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^\n>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^\n<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>\n^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>\nv^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"
+        val parts = input.split("\n\n")
+        val (_grid, _robotPos) = Day15.parseGrid(parts(0))
+        val (grid, robotPos) = Day15.widenGrid(_grid, _robotPos)
+        val moves = Day15.parseMoves(parts(1))
+        println(Day15.dumpGrid(grid))
+        assertEquals(Day15.part2(grid, robotPos, moves), 9021)
+    }
