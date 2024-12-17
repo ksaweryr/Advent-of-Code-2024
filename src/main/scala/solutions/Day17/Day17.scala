6707fb962b2d9658662885b7fb31b159ddce70fb
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-17 11:23:23 +0100

    Day 17 part 2

--- src/main/scala/solutions/Day17/Day17.scala
+++ src/main/scala/solutions/Day17/Day17.scala
@@ -5,21 +5,33 @@ import scala.annotation.tailrec
 def solve(input: String): Unit =
     val (state, program) = parse(input)
     println(part1(state, program))
+    println(part2(input.split("\n\n")(1).strip.split(": ")(1).split(",").map(_.toLong)))
 
 def part1(state: ProgramState, program: Array[Instruction]): String =
     runProgram(state, program).output.reverse.mkString(",")
 
+def part2(numbers: Array[Long]): Long =
+    // there is one jump - as the last instruction, it jumps to the front
+    // B and C are set based only on the value of few lowest bits of A at the start of every iteration
+    // in every iteration A is divided by 8
+    val program = parseProgram(numbers)
+    (0 to numbers.length - 1).foldLeft(List(0L))((acc, i) =>
+        acc.flatMap(ac => (for { a <- 0 to 7 } yield
+            ((ac << 3) | a, runProgram(ProgramState(0, (ac << 3) | a, 0, 0, List.empty), program.init).output.head == numbers(numbers.length - 1 - i)))
+        .filter(_._2)
+        .map(_._1))).min
+
 def parse(s: String): (ProgramState, Array[Instruction]) =
     val parts = s.split("\n\n")
     val regs = parts(0).split("\n").map(_.split(": ")(1))
-    val state = ProgramState(0, regs(0).toInt, regs(1).toInt, regs(2).toInt, List.empty)
-    val program = parseProgram(parts(1).split(": ")(1).strip.split(",").map(_.toInt))
+    val state = ProgramState(0, regs(0).toLong, regs(1).toLong, regs(2).toLong, List.empty)
+    val program = parseProgram(parts(1).split(": ")(1).strip.split(",").map(_.toLong))
     (state, program)
 
-def parseProgram(program: Array[Int]): Array[Instruction] =
+def parseProgram(program: Array[Long]): Array[Instruction] =
     program.grouped(2).map(a => parseInstruction(a(0), a(1))).toArray
 
-def parseInstruction(instruction: Int, operand: Int): Instruction =
+def parseInstruction(instruction: Long, operand: Long): Instruction =
     instruction match
         case 0 => Instruction(Opcode.Adv, parseComboOperand(operand))
         case 1 => Instruction(Opcode.Bxl, Immediate(operand))
@@ -31,7 +43,7 @@ def parseInstruction(instruction: Int, operand: Int): Instruction =
         case 7 => Instruction(Opcode.Cdv, parseComboOperand(operand))
         case _ => throw RuntimeException("Should never happen")
 
-def parseComboOperand(operand: Int): Operand =
+def parseComboOperand(operand: Long): Operand =
     operand match
         case 0 | 1 | 2 | 3 => Immediate(operand)
         case 4 => RegisterA()
@@ -46,17 +58,17 @@ def parseComboOperand(operand: Int): Operand =
         val instruction = program(state.pc)
         val operand = evaluateOperand(state, instruction.operand)
         val newState = instruction.opcode match
-            case Opcode.Adv => state.copy(pc = state.pc + 1, ra = state.ra / (1 << operand))
+            case Opcode.Adv => state.copy(pc = state.pc + 1, ra = state.ra >> operand)
             case Opcode.Bxl => state.copy(pc = state.pc + 1, rb = state.rb ^ operand)
             case Opcode.Bst => state.copy(pc = state.pc + 1, rb = operand & 7)
-            case Opcode.Jnz => state.copy(pc = if state.ra == 0 then state.pc + 2 else operand / 2)
+            case Opcode.Jnz => state.copy(pc = if state.ra == 0 then state.pc + 2 else (operand >> 1L).toInt)
             case Opcode.Bxc => state.copy(pc = state.pc + 1, rb = state.rb ^ state.rc)
-            case Opcode.Out => state.copy(pc = state.pc + 1, output = (operand & 7) :: state.output)
+            case Opcode.Out => state.copy(pc = state.pc + 1, output = (operand & 7).toInt :: state.output)
             case Opcode.Bdv => state.copy(pc = state.pc + 1, rb = state.ra / (1 << operand))
             case Opcode.Cdv => state.copy(pc = state.pc + 1, rc = state.ra / (1 << operand))
         runProgram(newState, program)
 
-def evaluateOperand(state: ProgramState, operand: Operand): Int =
+def evaluateOperand(state: ProgramState, operand: Operand): Long =
     operand match
         case Immediate(x) => x
         case RegisterA() => state.ra
@@ -64,7 +76,7 @@ def evaluateOperand(state: ProgramState, operand: Operand): Int =
         case RegisterC() => state.rc
     
 
-case class ProgramState(val pc: Int, val ra: Int, val rb: Int, val rc: Int, val output: List[Int])
+case class ProgramState(val pc: Int, val ra: Long, val rb: Long, val rc: Long, val output: List[Int])
 
 case class Instruction(val opcode: Opcode, val operand: Operand)
 
@@ -72,7 +84,7 @@ enum Opcode:
     case Adv, Bxl, Bst, Jnz, Bxc, Out, Bdv, Cdv
 
 sealed trait Operand
-case class Immediate(val x: Int) extends Operand
+case class Immediate(val x: Long) extends Operand
 case class RegisterA() extends Operand
 case class RegisterB() extends Operand
 case class RegisterC() extends Operand
