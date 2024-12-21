0c7062f1250ddc184077edce6a8915f854a0470a
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-21 22:51:27 +0100

    Day 21 part 2

--- src/main/scala/solutions/Day21/Day21.scala
+++ src/main/scala/solutions/Day21/Day21.scala
@@ -3,30 +3,38 @@ package solutions.Day21
 def solve(input: String): Unit =
     val codes = input.split("\n")
     println(part1(codes))
+    println(part2(codes))
 
-def part1(codes: Array[String]): Int =
-    codes.map(part1SingleCode).sum
+val part1 = solveForAllCodes(2)
+val part2 = solveForAllCodes(25)
 
-def part1SingleCode(code: String): Int =
-    val numericPart = code.init.toInt
-    numericPart * directionalToHigherLevel(directionalToHigherLevel(numericToHigherLevel(code.toStream))).length
+def solveForAllCodes(numIterations: Int)(codes: Array[String]): Long =
+    codes.map(solveForSingleCode(numIterations)).sum
 
-def numericToHigherLevel(code: Stream[Char], prevChar: Char = 'A'): Stream[Char] =
-    code match
-        case (head #:: tail) =>
-            lazy val res = numericShortestPath(prevChar, head).toStream ++ numericToHigherLevel(tail, head)
-            res
-        case Stream.Empty => Stream.empty[Char]
+def solveForSingleCode(numIterations: Int)(code: String): Long =
+    val numericPart = code.init.toLong
+    val dp = scala.collection.mutable.Map.empty[(Char, Char, Int), Long]
+    val seq = numericToHigherLevel(code)
+    numericPart * ('A' + seq).zip(seq).map((a, b) => shortestPathAtLevel(a, b, numIterations, dp)).sum
 
-def directionalToHigherLevel(keys: Stream[Char], prevChar: Char = 'A'): Stream[Char] =
-    keys match
-        case (head #:: tail) =>
-            lazy val res = directionalShortestPath(prevChar, head).toStream ++ directionalToHigherLevel(tail, head)
-            res
-        case Stream.Empty => Stream.empty[Char]
+def shortestPathAtLevel(from: Char, to: Char, level: Int, dp: scala.collection.mutable.Map[(Char, Char, Int), Long]): Long =
+    if level == 0 then
+        return 1L
+
+    if !dp.contains((from, to, level)) then
+        val path = directionalShortestPath(from, to)
+        val cost = ('A' + path).zip(path).map((a, b) => shortestPathAtLevel(a, b, level - 1, dp)).sum
+        dp.update((from, to, level), cost)
+    dp((from, to, level))
+
+def numericToHigherLevel(code: String, prevChar: Char = 'A'): String =
+    if code.length == 0 then
+        ""
+    else
+        numericShortestPath(prevChar, code.head) + numericToHigherLevel(code.tail, code.head)
 
 def numericShortestPath(from: Char, to: Char): String =
-    val (dx, dy) = shortestPath(from, to, numericKeysLocations)
+    val (dx, dy) = distanceBetweenKeys(from, to, numericKeysLocations)
     val hor = if dx > 0 then ">" * dx else "<" * -dx
     val ver = if dy > 0 then "v" * dy else "^" * -dy
     val horFirstLegal = {
@@ -38,8 +46,8 @@ def numericShortestPath(from: Char, to: Char): String =
         dy != 0 && (x != 0 || y + dy != 3)
     }
     if horFirstLegal && verFirstLegal then
-        val horADist = shortestPath(hor(0), 'A', directionalKeysLocations)
-        val verADist = shortestPath(ver(0), 'A', directionalKeysLocations)
+        val horADist = distanceBetweenKeys(hor(0), 'A', directionalKeysLocations)
+        val verADist = distanceBetweenKeys(ver(0), 'A', directionalKeysLocations)
         val horFirstCost = verADist._1.abs + verADist._2.abs
         val verFirstCost = horADist._1.abs + horADist._2.abs
         if horFirstCost < verFirstCost then
@@ -52,7 +60,7 @@ def numericShortestPath(from: Char, to: Char): String =
         ver + hor + 'A'
 
 def directionalShortestPath(from: Char, to: Char): String =
-    val (dx, dy) = shortestPath(from, to, directionalKeysLocations)
+    val (dx, dy) = distanceBetweenKeys(from, to, directionalKeysLocations)
     val hor = if dx > 0 then ">" * dx else "<" * -dx
     val ver = if dy > 0 then "v" * dy else "^" * -dy
     val horFirstLegal = {
@@ -64,8 +72,8 @@ def directionalShortestPath(from: Char, to: Char): String =
         dy != 0 && (x != 0 || y + dy != 0)
     }
     if horFirstLegal && verFirstLegal then
-        val horADist = shortestPath(hor(0), 'A', directionalKeysLocations)
-        val verADist = shortestPath(ver(0), 'A', directionalKeysLocations)
+        val horADist = distanceBetweenKeys(hor(0), 'A', directionalKeysLocations)
+        val verADist = distanceBetweenKeys(ver(0), 'A', directionalKeysLocations)
         val horFirstCost = verADist._1.abs + verADist._2.abs
         val verFirstCost = horADist._1.abs + horADist._2.abs
         if horFirstCost < verFirstCost then
@@ -77,7 +85,7 @@ def directionalShortestPath(from: Char, to: Char): String =
     else
         ver + hor + 'A'
 
-def shortestPath(from: Char, to: Char, movementMap: Map[Char, (Int, Int)]): (Int, Int) =
+def distanceBetweenKeys(from: Char, to: Char, movementMap: Map[Char, (Int, Int)]): (Int, Int) =
     val (x0, y0) = movementMap(from)
     val (x1, y1) = movementMap(to)
     val dx = x1 - x0
