4ab8ab71d39b5b9e34b4feb4644119d155a128c3
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-23 10:31:39 +0100

    Day 23 part 1

--- src/main/scala/solutions/Day23/Day23.scala
+++ src/main/scala/solutions/Day23/Day23.scala
@@ -1,4 +1,32 @@
 package solutions.Day23
 
+import scala.collection.{mutable => M}
+
 def solve(input: String): Unit =
-    println("TBI!")
+    val g = parseGraph(input)
+    println(part1(g))
+
+def part1(graph: Map[String, Set[String]]): Int =
+    graph.toList
+        .flatMap((a, s) => s.map(b => (a, b)))
+        .map((a, b) => graph.keys.count(c => graph(c).contains(a) && graph(c).contains(b) && (a(0) == 't' || b(0) == 't' || c(0) == 't'))).sum / 6
+
+def parseGraph(input: String): Map[String, Set[String]] =
+    constructGraph(parseEdges(input))
+
+def parseEdges(input: String): Vector[(String, String)] =
+    input.split("\n").map(row => {
+        val parts = row.split("-")
+        (parts(0), parts(1))
+    }).toVector
+
+def constructGraph(edges: Vector[(String, String)]): Map[String, Set[String]] =
+    val m = M.Map.empty[String, M.Set[String]]
+    for (a, b) <- edges do
+        if !m.contains(a) then
+            m.update(a, M.Set.empty[String])
+        if !m.contains(b) then
+            m.update(b, M.Set.empty[String])
+        m(a).add(b)
+        m(b).add(a)
+    m.map((k, v) => (k, v.toSet)).toMap
--- src/test/scala/Day23.scala
+++ src/test/scala/Day23.scala
@@ -1,4 +1,8 @@
 import solutions.Day23
 
 class Day23 extends munit.FunSuite:
-    
+    test("Part 1 example") {
+        val input = "kh-tc\nqp-kh\nde-cg\nka-co\nyn-aq\nqp-ub\ncg-tb\nvc-aq\ntb-ka\nwh-tc\nyn-cg\nkh-ub\nta-co\nde-co\ntc-td\ntb-wq\nwh-td\nta-ka\ntd-qp\naq-cg\nwq-ub\nub-vc\nde-ta\nwq-aq\nwq-vc\nwh-yn\nka-de\nkh-ta\nco-tc\nwh-qp\ntb-vc\ntd-yn"
+        val g = Day23.parseGraph(input)
+        assertEquals(Day23.part1(g), 7)
+    }
