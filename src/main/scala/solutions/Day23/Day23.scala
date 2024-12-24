9fe1e8743ca050313df57d5e4c9c708f71971ef9
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-24 12:09:04 +0100

    Day 23 part 2

--- src/main/scala/solutions/Day23/Day23.scala
+++ src/main/scala/solutions/Day23/Day23.scala
@@ -5,11 +5,24 @@ import scala.collection.{mutable => M}
 def solve(input: String): Unit =
     val g = parseGraph(input)
     println(part1(g))
+    println(part2(g))
 
 def part1(graph: Map[String, Set[String]]): Int =
     graph.toList
         .flatMap((a, s) => s.map(b => (a, b)))
         .map((a, b) => graph.keys.count(c => graph(c).contains(a) && graph(c).contains(b) && (a(0) == 't' || b(0) == 't' || c(0) == 't'))).sum / 6
+
+def part2(graph: Map[String, Set[String]]): String =
+    graph.keys.map(maxClique(graph, _)).maxBy(_.size).toList.sorted.mkString(",")
+
+def maxClique(graph: Map[String, Set[String]], v: String): Set[String] =
+    powerset(graph(v).toSeq).find(isClique(graph, _)).get + v
+
+def isClique(graph: Map[String, Set[String]], clique: Set[String]): Boolean =
+    clique.forall(a => clique.forall(b => a == b || graph(a).contains(b)))
+
+def powerset[A](s: Seq[A]): IndexedSeq[Set[A]] =
+    (0 to s.size).reverse.flatMap(s.combinations).map(_.toSet)
 
 def parseGraph(input: String): Map[String, Set[String]] =
     constructGraph(parseEdges(input))
