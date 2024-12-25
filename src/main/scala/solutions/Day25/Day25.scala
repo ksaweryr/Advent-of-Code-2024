f55b2c14bc58339ffdbac1db2f69ffd04d3df71c
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2024-12-25 11:49:33 +0100

    Day 25

--- src/main/scala/solutions/Day25/Day25.scala
+++ src/main/scala/solutions/Day25/Day25.scala
@@ -1,4 +1,30 @@
 package solutions.Day25
 
 def solve(input: String): Unit =
-    println("TBI!")
+    val (locks, keys) = parseInput(input)
+    println(part1(locks, keys))
+
+def parseInput(input: String): (Vector[Lock], Vector[Key]) =
+    val parts = input.split("\n\n")
+    val (locks, keys) = parts.map(parsePart).partition(_.isLeft)
+    (locks.map(_.left.get).toVector, keys.map(_.right.get).toVector)
+
+def parsePart(part: String): Either[Lock, Key] =
+    val rows = part.split("\n")
+    val transposed = for x <- 0 to rows(0).length - 1 yield
+        for y <- 0 to rows.length - 1 yield
+            rows(y)(x)
+    if transposed(0)(0) == '#' then
+        Left(Lock(transposed.map(col => col.indexOf('.') - 1).toVector))
+    else
+        Right(Key(transposed.map(col => col.reverse.indexOf('.') - 1).toVector))
+
+def part1(locks: Vector[Lock], keys: Vector[Key]): Int =
+    (for lock <- locks; key <- keys yield
+        fits(key, lock)).count(p => p)
+        
+def fits(key: Key, lock: Lock): Boolean =
+    key.pins.zip(lock.pins).map(_ + _).forall(_ <= 5)
+
+case class Lock(val pins: Vector[Int])
+case class Key(val pins: Vector[Int])
--- src/test/scala/Day25.scala
+++ src/test/scala/Day25.scala
@@ -1,4 +1,22 @@
 import solutions.Day25
 
 class Day25 extends munit.FunSuite:
-    
+    test("Parse lock") {
+        val part = "#####\n.####\n.####\n.####\n.#.#.\n.#...\n....."
+        val lock = Day25.parsePart(part)
+        assert(lock.isLeft)
+        assertEquals(lock.left.get.pins, Vector(0, 5, 3, 4, 3))
+    }
+
+    test("Parse key") {
+        val part = ".....\n#....\n#....\n#...#\n#.#.#\n#.###\n#####"
+        val key = Day25.parsePart(part)
+        assert(key.isRight)
+        assertEquals(key.right.get.pins, Vector(5, 0, 2, 1, 3))
+    }
+
+    test("Part 1 example") {
+        val input = "#####\n.####\n.####\n.####\n.#.#.\n.#...\n.....\n\n#####\n##.##\n.#.##\n...##\n...#.\n...#.\n.....\n\n.....\n#....\n#....\n#...#\n#.#.#\n#.###\n#####\n\n.....\n.....\n#.#..\n###..\n###.#\n###.#\n#####\n\n.....\n.....\n.....\n#....\n#.#..\n#.#.#\n#####"
+        val (locks, keys) = Day25.parseInput(input)
+        assertEquals(Day25.part1(locks, keys), 3)
+    }
